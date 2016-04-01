7059af9ec4cebc74d88063ce20091add0264a20c
sahilgupta202 <sahil.gupta@comprotechnologies.com>
2016-04-01 13:31:43 +0530

    Initial Commit

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/../client'));
 
-app.listen(8080, function () {
+app.listen(port, function () {
   console.log('app listening on port 8080.');
 });
